f4501e185e003a8e32fcbd5f7f3faf70f3403a8f
j0n1que <Yevgeniyshc@mail.ru>
2024-10-19 13:37:12 +0300

    func to generate new link in string format done

---
+++ incrementer.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+const (
+	alph = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+)
+
+func Find(c uint8) (int, error) {
+	for i := range alph {
+		if c == alph[i] {
+			return i, nil
+		}
+	}
+	return -1, fmt.Errorf("Symbol not found")
+}
+
+func Increment(oldString string) (string, error) {
+	newString := []byte(oldString)
+	i := len(oldString) - 1
+	n := len(alph)
+	for i > -1 {
+		pos, err := Find(newString[i])
+		if err != nil {
+			return "", err
+		}
+		if pos != n-1 {
+			newString[i] = alph[pos+1]
+			i--
+			break
+		} else {
+			for i > 0 && pos == n-1 {
+				newString[i] = alph[0]
+				i--
+				pos, err = Find(newString[i])
+				if err != nil {
+					return "", err
+				}
+			}
+			if pos == n-1 {
+				return "", fmt.Errorf("Overflow")
+			}
+			newString[i] = alph[pos+1]
+			break
+		}
+	}
+	return string(newString), nil
+}
